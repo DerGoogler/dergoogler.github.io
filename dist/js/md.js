0f60acfb525d0e25866d779b300aaf6402ae8979
Der_Googler™ <54764558+DerGoogler@users.noreply.github.com>
2020-11-30 22:23:23 +0100

    Update md.js

--- dist/js/md.js
+++ dist/js/md.js
@@ -1,6 +1,7 @@
 function reqListener() {
     function parseDLGM(dlgmText) {
         const dlg = dlgmText
+            .replace(/\[fa-icon\((.*?)\)\]/gim, "<i class='fab fa-$1'></i>>")
             .replace(/\[x\]/gim, "<input type='checkbox' id='coding' name='interest' value='coding' checked disabled>")
             .replace(/\[ \]/gim, "<input type='checkbox' id='coding' name='interest' value='coding' disabled>")
             .replace(/\[download\((.*?)\)\((.*?)\)\]/gim, "<a class='btn2' href='$2'>$1</a>")
@@ -9,7 +10,14 @@ function reqListener() {
             // [svg-badge(Build)(Android Studio)(272727)]
             .replace(/\[svg-badge\((.*?)\)\((.*?)\)\((.*?)\)\]/gim, "<svg width='133.3' height='20' viewBox='0 0 1333 200' xmlns='http://www.w3.org/2000/svg' role='img' aria-label='$1 : $2'><g><rect fill='#555' width='413' height='200'/><rect fill='#$3' x='413' width='920' height='200'/></g><g aria-hidden='true' fill='#fff' text-anchor='start' font-family='Verdana,DejaVu Sans,sans-serif' font-size='110'><text x='60' y='148' textLength='313' fill='#000' opacity='0.1'>$1</text><text x='50' y='138' textLength='313'>$1</text><text x='468' y='148' textLength='820' fill='#000' opacity='0.1'>$2</text><text x='458' y='138' textLength='820'>$2</text></g></svg>")
             //
-            .replace(/\[app-card\((.*?)\)\((.*?)\)\((.*?)\)\((.*?)\)\]/gim, "<div class='app-card'><img class='app-card-img' src='/dist/images/$3' alt='$1 - $2' style='width:100%'><div class='app-card-container'><h4><b><a href='$4'>$1</a></b></h4><p>$2</p></div></div>")
+            .replace(/\[app-card\((.*?)\)\((.*?)\)\((.*?)\)\((.*?)\)\((.*?)\)\]/gim, "<div class='app-card'><img class='app-card-img' src='/dist/images/$4' style='width:100%'><div class='app-card-container'><h4><b><a href='$5'>$1</a></b></h4><p>$2</p><p>$3</p></div></div>")
+            // fas
+            .replace(/\[playstore\]/gim, "<i class='fab fa-google-play'></i>")
+            .replace(/\[official\]/gim, "<i class='fas fa-certificate'></i>")
+            .replace(/\[promo\]/gim, "<i class='fas fa-ad'></i>")
+            .replace(/\[web\]/gim, "<i class='fas fa-globe-americas'></i>")
+            .replace(/\[android\]/gim, "<i class='fab fa-android'></i>")
+
 
 
 
